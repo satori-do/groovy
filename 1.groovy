5d64ecdeb6c5e3aad9be1778c00ab7e22618286e
satori-do <vladimir.reznichenko@onix-systems.com>
2018-07-20 17:50:12 +0300

    DEVOPS-01. My first dual example on python & groovy

---
+++ 1.groovy
@@ -0,0 +1,13 @@
+#! /home/satori/.sdkman/candidates/groovy/current/bin/groovy
+
+println("Hello World!!!");
+
+// Function definiution is here
+def printme(String str){
+    println str
+}
+
+// Now you can call printme Function
+printme "I'm first call for user defined function!"
+printme 'Again second call to the same function'
+printme ''
---
+++ 2.groovy
@@ -0,0 +1,16 @@
+#! /home/satori/.sdkman/candidates/groovy/current/bin/groovy
+
+// First function definiution is here
+def printme(String str, String str2){
+    println str + str2
+}
+// Now you can call printme Function
+printme ("I'm first call for user defined function!", ' It is cool!')
+
+
+//Second function definition is here
+def sumer(int did1, int did2){
+    println did1 + did2
+}
+// Now you can call sumer Function
+sumer (2, 2)
