fcf8192ecc2c2f227890e0b8fb27f8c725f3e6c7
satori-do <vladimir.reznichenko@onix-systems.com>
2018-08-15 12:42:57 +0300

    DEVOPS-07. Get name from URL

--- 1.groovy
+++ 1.groovy
@@ -1,15 +1,23 @@
 #! /home/satori/.sdkman/candidates/groovy/current/bin/groovy
 
-println("Hello World!!!");
+// println("Hello World!!!");
+//
+// // Function definiution is here
+// def printme(String str){
+//   for (i = 0; i <3; i++) {
+//     println str
+//   }
+// }
+//
+// // Now you can call printme Function
+// printme "I'm first call for user defined function!"
+// printme 'Again second call to the same function'
+// printme ''
 
-// Function definiution is here
-def printme(String str){
-  for (i = 0; i <3; i++) {
-    println str
-  }
-}
+//*GET URL NAME*////////////////////////////////////////////////////////////////
 
-// Now you can call printme Function
-printme "I'm first call for user defined function!"
-printme 'Again second call to the same function'
-printme ''
+URI dbUri = new URI('http://google.com/?q=URL')
+ def host = dbUri?.getHost()
+ println "${host}"
+
+////////////////////////////////////////////////////////////////////////////////
